8e77caf93632885d60c81b76842350dd521b9156
Burush0 <burush02@gmail.com>
2025-03-15 21:43:54 +0100

    Added simple REPL

--- repl.go
+++ repl.go
@@ -1,9 +1,27 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
 
 func cleanInput(text string) []string {
 	return strings.Fields(strings.ToLower(text))
+}
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		words := cleanInput(scanner.Text())
+		if len(words) == 0 {
+			continue
+		}
+
+		output := fmt.Sprintf("Your command was: %s", words[0])
+		fmt.Println(output)
+	}
 }
