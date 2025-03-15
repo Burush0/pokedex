79584f441b2e739b912bd46fc3cc20ffd5830ec5
Burush0 <burush02@gmail.com>
2025-03-15 22:03:45 +0100

    Added support for help and exit arguments in the REPL

--- repl.go
+++ repl.go
@@ -7,10 +7,6 @@ import (
 	"strings"
 )
 
-func cleanInput(text string) []string {
-	return strings.Fields(strings.ToLower(text))
-}
-
 func startRepl() {
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
@@ -21,7 +17,43 @@ func startRepl() {
 			continue
 		}
 
-		output := fmt.Sprintf("Your command was: %s", words[0])
-		fmt.Println(output)
+		commandName := words[0]
+
+		cmd, ok := getCommands()[commandName]
+		if ok {
+			err := cmd.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Println("Unknown command")
+			continue
+		}
+	}
+}
+
+func cleanInput(text string) []string {
+	return strings.Fields(strings.ToLower(text))
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
 	}
 }
